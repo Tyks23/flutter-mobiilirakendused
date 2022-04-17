d651108e317eb2df41982f19be88dd0b21fda3f3
kaarelel <70900290+kaarelel@users.noreply.github.com>
2022-04-17 15:02:07 +0300

    Add files via upload

--- 7/test_project/lib/httpServices.dart
+++ 7/test_project/lib/httpServices.dart
@@ -7,16 +7,29 @@ class HttpService {
 
   Future<List<Post>> getPosts() async {
     api.Response res = await api.get(Uri.parse(postsURL));
+
     if (res.statusCode == 200) {
       List<dynamic> body = jsonDecode(res.body)["data"];
+
       List<Post> posts = body
           .map(
             (dynamic item) => Post.fromJson(item),
       )
           .toList();
+
       return posts;
     } else {
       throw "Unable to retrieve posts.";
+    }
+  }
+
+  Future<void> deletePost(int id) async {
+    api.Response res = await api.delete(Uri.parse("$postsURL/$id"));
+
+    if (res.statusCode == 200) {
+      print("DELETED");
+    } else {
+      throw "Unable to delete post.";
     }
   }
 
